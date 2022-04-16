9adb1152de23a72baad6db045d942a77c0d8e310
den8383arfp <den8383arfp@gmail.com>
2022-04-17 00:07:10 +0900

    first commit

---
+++ config.fish
@@ -0,0 +1,25 @@
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+
+
+set PATH /usr/local/bin $PATH
+set PATH /usr/sbin $PATH
+set PATH ~/.local/bin $PATH
+set PATH ~/flutter/bin $PATH
+set PATH ~/Android/Sdk/emulator $PATH
+
+alias ori-vim "/usr/bin/vim"
+alias vim "nvim"
+
+abbr -a adb /Android/Sdk/platform-tools
+abbr -a ll ls -alF
+abbr -a la ls -A
+abbr -a l ls -CF
+abbr -a .. cd ../
+abbr -a ... cd ../../
+abbr -a .... cd ../../../
+abbr -a - cd -
+
+
+set -g theme_color_scheme gruvbox
+
